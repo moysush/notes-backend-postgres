02bada48dc628e1fe07809019fca2b9a0bf5e3e9
Mahmud Hossain Sushmoy <sushmoy@duck.com>
2026-05-14 18:58:06 +0600

    add and modify the endpoints

--- index.js
+++ index.js
@@ -40,11 +40,23 @@ Note.init(
     modelName: "note",
   },
 );
+Note.sync()
 
 app.get("/api/notes", async (req, res) => {
   const notes = await Note.findAll();
+  console.log(JSON.stringify(notes, null, 2));
   res.json(notes);
 });
+
+app.get("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id)
+  if (note) {
+    console.log(note.toJSON());
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
 
 app.post("/api/notes", async (req, res) => {
   try {
@@ -55,6 +67,17 @@ app.post("/api/notes", async (req, res) => {
     return res.status(400).json({ error });
   }
 });
+
+app.put('/api/notes/:id', async (req, res) => {
+  const note = await Note.findByPk(req.params.id)
+  if (note) {
+    note.important = req.body.important
+    await note.save()
+    res.json(note)
+  } else {
+    res.status(404).end()
+   }
+})
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
